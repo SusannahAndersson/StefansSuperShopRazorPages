c391227a78c82c30f814f28a92c7bf0e6d51d292
andreashallerod <andreas.hallerod@learnet.se>
2022-12-14 15:25:45 +0100

    added newsletter and subscriber model
    added newsletter and subscriber model

---
+++ StefansSuperShop/Data/NewsletterSubscriber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StefansSuperShop.Data
+{
+	public class NewsletterSubscriber
+	{
+		public NewsletterSubscriber()
+		{
+            this.Newsletters = new HashSet<Newsletter>();
+        }
+
+        [Key]
+        [Column("NewsletterSubscriberID")]
+        public int NewsletterSubscriberId { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Mail { get; set; }
+        public virtual ICollection<Newsletter> Newsletters { get; set; }
+    }
+}
+
