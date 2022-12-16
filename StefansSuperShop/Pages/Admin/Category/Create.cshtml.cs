d4d70496c6eb4a7097487d0005420e4676b8879c
SusannahAndersson <susannah.alx@gmail.com>
2022-12-16 12:06:43 +0100

    Add new category

---
+++ StefansSuperShop/Pages/Admin/Category/Create.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StefansSuperShop.Data;
+using System.Threading.Tasks;
+
+namespace StefansSuperShop.Pages.Admin.Category
+{
+    public class CreateModel : PageModel
+    {
+        private readonly StefansSuperShop.Data.ApplicationDbContext _context;
+
+        public CreateModel(StefansSuperShop.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Categories Categories { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Categories.Add(Categories);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
